d5fac29693bf3aa0ce5ec4291a60a380088d1f8b
Jonathan Robichaud <jonathanrobichaud@icloud.com>
2024-03-24 02:08:17 -0300

    fix py

--- esphome/components/levoit/light/__init__.py
+++ esphome/components/levoit/light/__init__.py
@@ -2,8 +2,8 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import light
 from esphome.const import (
-    CONF_OUTPUT_ID
-)
+     ICON_LIGHTBULB, CONF_LIGHT
+ )
 
 from .. import levoit_ns, CONF_LEVOIT_ID, Levoit
 
@@ -11,19 +11,19 @@ DEPENDENCIES = ["levoit"]
 CODEOWNERS = ["@acvigue"]
 
 LevoitLight = levoit_ns.class_("LevoitLight", cg.Component, light.LightOutput)
+LevoitLightPurpose = levoit_ns.enum("LevoitLightPurpose")
 #LevoitLight = levoit_ns.class_('LevoitLight', light.LightOutput)
 
-CONFIG_SCHEMA = light.BRIGHTNESS_ONLY_LIGHT_SCHEMA.extend(
-    {
-        cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(LevoitLight),
+CONFIG_SCHEMA = (
+    cv.Schema({
         cv.GenerateID(CONF_LEVOIT_ID): cv.use_id(Levoit),
-    }
-).extend(cv.COMPONENT_SCHEMA)
-
+        cv.Optional(CONF_LIGHT): light.BRIGHTNESS_ONLY_LIGHT_SCHEMA(LevoitLight, icon=ICON_LIGHTBULB).extend(cv.COMPONENT_SCHEMA),
+    })
+)
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_LEVOIT_ID])
 
-    var = cg.new_Pvariable(config[CONF_OUTPUT_ID], parent)
-    await cg.register_component(var, config)
-    await light.register_light(var, config)
+    if config_light := config.get(CONF_LIGHT):
+        var = await light.new_light(config_light, parent, LevoitLightPurpose.LIGHT)
+        await cg.register_component(var, config_light)
