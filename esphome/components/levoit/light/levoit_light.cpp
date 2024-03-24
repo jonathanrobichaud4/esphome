9c38d2f372585ec80744583d16b20d1318139377
Jonathan Robichaud <jonathanrobichaud@icloud.com>
2024-03-24 20:28:30 -0300

    test2

--- esphome/components/levoit/light/levoit_light.cpp
+++ esphome/components/levoit/light/levoit_light.cpp
@@ -29,7 +29,7 @@ void LevoitLight::setup() {
       //call.set_publish(true);
       //call.set_brightness(brightness);
       call.perform();
-      this->state_->publish_state();
+      //this->state_->publish_state();
      });
    }
     
