dfd8cef6c82deac9dedb6b0712ab16c32c86e6bd
Jonathan Robichaud <jonathanrobichaud@icloud.com>
2024-03-26 15:18:44 -0300

    test

--- esphome/components/levoit/number/levoit_number.h
+++ esphome/components/levoit/number/levoit_number.h
@@ -3,13 +3,14 @@
 #include "esphome/core/component.h"
 #include "esphome/components/levoit/levoit.h"
 #include "esphome/components/number/number.h"
+#include "esphome/components/output/float_output.h"
 
 namespace esphome {
 namespace levoit {
 
 enum LevoitNumberPurpose: uint8_t { HUMIDITY_LEVEL, LIGHT};
 
-class LevoitNumber : public number::Number, public Component {
+class LevoitNumber : public number::Number, public Component, output::FloatOutput {
  public:
   LevoitNumber(Levoit *parent, LevoitNumberPurpose purpose) : parent_(parent), purpose_(purpose) {}
   void setup() override;
