3a705cc9fb848c881ad9ae464e412f7a715c5fb1
Jonathan Robichaud <jonathanrobichaud@icloud.com>
2024-03-26 03:14:49 -0300

    test

--- esphome/components/levoit/light/levoit_light.cpp
+++ esphome/components/levoit/light/levoit_light.cpp
@@ -18,6 +18,7 @@ void LevoitLight::setup() {
         ESP_LOGD(TAG, "Light is transitioning, datapoint change ignored");
         return;
       }
+      else{
       auto call = this->state_->make_call();
       if (brightness == 0) {
         call.set_state(false);
@@ -26,6 +27,7 @@ void LevoitLight::setup() {
         call.set_state(true);
         call.set_brightness(brightness);
         call.perform();
+      }
       }
       //call.set_publish(true);
       //call.set_brightness(brightness);
